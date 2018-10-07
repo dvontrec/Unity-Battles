b19be2f7eb3c9026d341400339315c1121606e94
Dvontre Coleman <dvontrec@gmail.com>
2018-10-07 13:52:52 -0400

    Add flashlight activity

--- WIP Battles/Assets/_Scripts/PlayerMoveScript.cs
+++ WIP Battles/Assets/_Scripts/PlayerMoveScript.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMoveScript : MonoBehaviour {
-
+	public float moveSpeed;
+	public float rotationSpeed;
+	public GameObject flashlight;
+	public bool flashlightIsActive = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,13 +14,23 @@ public class PlayerMoveScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float moveVert = Input.GetAxis("Vertical") * 5f * Time.deltaTime;
-		float moveHorz = Input.GetAxis("Horizontal") * 5f * Time.deltaTime;
-		transform.Translate(moveHorz, 0.0f, moveVert);
+		float moveVert = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+		float moveHorz = Input.GetAxis("Horizontal") * rotationSpeed* Time.deltaTime;
+		transform.Translate(0.0f, 0.0f, moveVert);
+		transform.Rotate(0.0f, moveHorz, 0.0f);
+
+		if(Input.GetKeyDown("space")){
+			ToggleLight();
+		}
 
 	}
 
 	void OnTriggerEnter(Collider Other){
 		Destroy(Other.gameObject);
+	}
+
+	void ToggleLight() {
+		flashlight.SetActive(!flashlightIsActive);
+		flashlightIsActive = !flashlightIsActive;
 	}
 }
